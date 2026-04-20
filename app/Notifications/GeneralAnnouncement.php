4bd4321da04ddcde7fd2cdfb2cb3b4346d9a10cd
ROY KEVIN <rbonifaciof@unamad.edu.pe>
2026-04-20 16:55:46 -0500

    Optimización masiva: Activación de Queues para 3k+ usuarios y mejora de visibilidad Heads-up en Android

--- app/Notifications/GeneralAnnouncement.php
+++ app/Notifications/GeneralAnnouncement.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class GeneralAnnouncement extends Notification
+class GeneralAnnouncement extends Notification implements ShouldQueue
 {
     use Queueable;
 
