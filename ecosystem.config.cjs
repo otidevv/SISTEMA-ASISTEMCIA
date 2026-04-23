1db065fbd86239b8738dc4e65dc5619d7b2e4dcb
ROY KEVIN <rbonifaciof@unamad.edu.pe>
2026-04-23 11:46:32 -0500

    Solucionado error ASLR de PHP en Windows para PM2

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,10 +2,11 @@ module.exports = {
   apps : [{
     name: 'asistencia-realtime',
     script: 'php',
-    args: 'artisan asistencia:daemon',
+    args: '-d opcache.enable=0 artisan asistencia:daemon',
     instances: 1,
     autorestart: true,
     watch: false,
     max_memory_restart: '1G'
   }]
 };
+
