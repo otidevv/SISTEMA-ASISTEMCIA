b3d868794bfc403576a23947009132077498bc08
ROY KEVIN <rbonifaciof@unamad.edu.pe>
2026-04-28 11:28:07 -0500

    Ajuste de posición, opacidad y tamaño del patrón Kené en la Constancia de Inscripción para mejorar su visibilidad

--- resources/views/pdf/constancia-postulacion.blade.php
+++ resources/views/pdf/constancia-postulacion.blade.php
@@ -249,15 +249,15 @@
         /* Marca de Agua con Tejido Institucional */
         .watermark-tejido {
             position: fixed;
-            top: -10%;
+            top: -30%;
             left: -10%;
             width: 120%;
-            height: 120%;
+            height: 140%;
             z-index: -2000;
-            opacity: 0.05;
+            opacity: 0.20;
             background-image: url('{{ public_path("assets_cepre/img/tejido-kene-final.png") }}');
             background-repeat: repeat;
-            background-size: 400px;
+            background-size: 450px;
         }
     </style>
 </head>
