bc689913f6650cd75c16f54a9d630206af35afba
ROY KEVIN <rbonifaciof@unamad.edu.pe>
2026-06-29 12:43:09 -0500

    chore(pm2): agregar asistencia-queue y asistencia-reverb a la configuracion de ecosystem

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,12 +1,32 @@
 module.exports = {
-  apps : [{
-    name: 'asistencia-realtime',
-    script: 'php',
-    args: '-d opcache.enable=0 artisan asistencia:daemon',
-    instances: 1,
-    autorestart: true,
-    watch: false,
-    max_memory_restart: '1G'
-  }]
+  apps : [
+    {
+      name: 'asistencia-realtime',
+      script: 'php',
+      args: '-d opcache.enable=0 artisan asistencia:daemon',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G'
+    },
+    {
+      name: 'asistencia-queue',
+      script: 'php',
+      args: 'artisan queue:work --sleep=3 --tries=3',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '512M'
+    },
+    {
+      name: 'asistencia-reverb',
+      script: 'node',
+      script: 'reverb.cjs',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '512M'
+    }
+  ]
 };
 
