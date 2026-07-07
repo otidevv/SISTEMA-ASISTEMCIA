be6dc3c276ff7a4a2bddfa3d6f199aa8c4bd66a2
ROY KEVIN <rbonifaciof@unamad.edu.pe>
2026-07-07 18:29:48 -0500

    fix: registrar el scheduler en bootstrap/app.php para Laravel 11 (Kernel.php estaba ignorado)

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Console\Scheduling\Schedule;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -17,6 +18,29 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->validateCsrfTokens(except: [
             'api/*',
         ]);
+    })
+    ->withSchedule(function (Schedule $schedule) {
+        // Procesar eventos de asistencia de estudiantes (cada minuto)
+        $schedule->command('asistencia:procesar-eventos')
+            ->withoutOverlapping()
+            ->everyMinute();
+
+        // Procesar eventos de asistencia de docentes (cada minuto)
+        $schedule->command('asistencia:procesar-docentes')
+            ->withoutOverlapping()
+            ->everyMinute();
+
+        // Recordatorio de temas pendientes y clases próximas (cada hora)
+        $schedule->command('notification:remind-teachers')
+            ->hourly();
+
+        // Agenda diaria para docentes - 07:00 AM
+        $schedule->command('asistencia:notificar-agenda')
+            ->dailyAt('07:00');
+
+        // Agenda del día siguiente - 08:00 PM
+        $schedule->command('asistencia:notificar-agenda --tomorrow')
+            ->dailyAt('20:00');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
